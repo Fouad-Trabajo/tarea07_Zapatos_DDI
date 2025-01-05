9becec885a8be10f328d10035dff98496fd31d9c
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2025-01-05 21:24:15 +0100

    feat: initial commit, first project

---
+++ app/src/main/java/com/fouadaha/tarea07zapatos/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.fouadaha.tarea07zapatos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.fouadaha.tarea07zapatos.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
