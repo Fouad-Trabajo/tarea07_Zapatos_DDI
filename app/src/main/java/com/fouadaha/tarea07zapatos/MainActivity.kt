44992517be4ea1db257725de7a9f0457af32f5bd
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2025-01-07 22:29:25 +0100

    feat: Ahora solo me falta ponerlo bonito y añadir la funcionalidad de la barra superior

--- app/src/main/java/com/fouadaha/tarea07zapatos/MainActivity.kt
+++ app/src/main/java/com/fouadaha/tarea07zapatos/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.fouadaha.tarea07zapatos
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,5 +15,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        functions()
+    }
+
+    private fun functions() {
+        binding.apply {
+            buttonSend.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Bottón 'enviar' pulsado", Toast.LENGTH_SHORT).show()
+            }
+            offerButton.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Bottón 'hacer una oferta' pulsado", Toast.LENGTH_SHORT).show()
+            }
+            buyButton.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Bottón 'comprar' pulsado", Toast.LENGTH_SHORT).show()
+            }
+            textMoreInformation.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Bottón 'Más' pulsado", Toast.LENGTH_SHORT).show()
+            }
+
+            checkbox.setOnCheckedChangeListener{ checkBox, isChecked ->
+                val likes = checkBox.text.toString().split(" ")[0].toIntOrNull() ?: 0
+                if(isChecked){
+                    checkBox.text = "${likes + 1} likes"
+                }else{
+                    checkBox.text = "${likes - 1} likes"
+
+                }
+
+            }
+        }
     }
 }
